36c34e909bc25d92553e511eb6d8b758cd6f1b4e
Cody Goeken <CodyGoeken@gmail.com>
2024-11-13 15:34:18 -0400

    Book model testing

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "jbuilder"
 
 gem "devise"
 
+gem 'rubocop', require: false
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:author) }
+    it { should validate_presence_of(:publication_year) }
+    it { should validate_presence_of(:isbn) }
+
+    it {
+      should allow_value('1234567890').for(:isbn)
+    }
+
+    it {
+      should allow_value('1234567890123').for(:isbn)
+    }
+
+    it {
+      should_not allow_value('12345').for(:isbn)
+    }
+
+    it {
+      should_not allow_value('invalid_isbn').for(:isbn)
+    }
+  end
+
+  describe 'custom validations' do
+    it 'raises an error if ISBN is invalid' do
+      book = Book.new(title: 'Test Book', author: 'Author Name', publication_year: 2023, isbn: '12345', user: User.new)
+      expect(book).not_to be_valid
+      expect(book.errors[:isbn]).to include("must be a valid ISBN")
+    end
+  end
 end
