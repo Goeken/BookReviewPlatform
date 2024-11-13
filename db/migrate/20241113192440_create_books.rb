81e0d1f158430e639218e8a14f69c2fcd4b2b6fa
Cody Goeken <CodyGoeken@gmail.com>
2024-11-13 15:29:00 -0400

    book model

---
+++ db/migrate/20241113192440_create_books.rb
@@ -0,0 +1,16 @@
+class CreateBooks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :publication_year
+      t.string :isbn
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :books, :title
+    add_index :books, :author
+  end
+end
