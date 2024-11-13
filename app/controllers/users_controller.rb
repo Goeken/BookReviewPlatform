4c2592d921e14e8fc02aabcd07df9eb153d9ca7c
Cody Goeken <CodyGoeken@gmail.com>
2024-11-13 19:32:07 -0400

    authentication views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_user!, only: [:show]
+
+  def show
+    @books = current_user.books
+    @reviews = current_user.reviews
+  end
+
+  private
+
+  def authorize_user!
+    unless current_user.id.to_s == params[:id]
+      redirect_to root_path, alert: 'You are not authorized to view that page'
+    end
+  end
+end
